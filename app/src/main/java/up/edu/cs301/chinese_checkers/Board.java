8d0cf73471cafad7b3574880e06ec0877972a57e
Luke Douville <luke.douville@gmail.com>
2017-10-17 15:07:01 -0700

    Fourth commit.  Switched back to using LinearLayouts.  Also added a board class to contain info for drawing the board.

---
+++ app/src/main/java/up/edu/cs301/chinese_checkers/Board.java
@@ -0,0 +1,30 @@
+package up.edu.cs301.chinese_checkers;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Color;
+import android.graphics.Path;
+
+/**
+ * Created by Luke D. Douville on 10/17/17.
+ */
+
+/*Class for drawing the board.*/
+public class Board
+{
+    protected float x, y;
+    protected Path board;
+    protected Paint myPaint;
+
+    public void draw(Canvas c)
+    {
+        myPaint.setColor(Color.BLACK);
+        /*Draw top triangle.*/
+        board.moveTo(500, 200);
+        board.lineTo(600, 100);
+        board.lineTo(700, 200);
+
+        /*After all Path points set:*/
+        c.drawPath(board, myPaint);
+    }
+}
--- app/src/main/java/up/edu/cs301/chinese_checkers/MainActivity.java
+++ app/src/main/java/up/edu/cs301/chinese_checkers/MainActivity.java
@@ -2,14 +2,23 @@ package up.edu.cs301.chinese_checkers;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+import android.view.SurfaceView;
+import android.graphics.Path;
 
 public class MainActivity extends AppCompatActivity
 {
+    private TextView turnTextView;
+    private TextView currentPlayerTextView;
+    private SurfaceView boardSurfaceView;
+//    private Path board;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        turnTextView = (TextView)findViewById(R.id.turnTextView);
+        currentPlayerTextView = (TextView)findViewById(R.id.currentPlayerTextView);
     }
 }
