64670d6faf5e2c212be0e551a56810a7c70c840e
sion18 <sion18@up.edu>
2017-11-09 17:49:25 -0800

    Added base GameState

--- app/src/main/java/up/edu/cs301/chinese_checkers/CCGameState.java
+++ app/src/main/java/up/edu/cs301/chinese_checkers/CCGameState.java
@@ -1,5 +1,9 @@
 package up.edu.cs301.chinese_checkers;
 
+import android.graphics.Color;
+
+import java.util.Hashtable;
+
 import up.edu.cs301.game.CCMainActivity;
 import up.edu.cs301.game.CCPlayer;
 import up.edu.cs301.game.infoMsg.CCInfo;
@@ -12,10 +16,37 @@ import up.edu.cs301.game.infoMsg.CCState;
 public class CCGameState extends CCState {
 
     //instance variables
+    Marble[][] board;
+    int id;
+    Hashtable<Integer,Color> ColorPlayerTable;
+
     public CCGameState(CCGameState state) {
         super();
     }
+    public Marble[][] getBoard(){
+        return board;
+    }
+    public int getId(){
+        return id;
+    }
+    public Hashtable<Integer,Color> getCPT(){
+        return ColorPlayerTable;
+    }
+
+    public void setBoard(Marble[][] b){
+        board = b;
+    }
+
+    public void setId(int i){
+        id = i;
+    }
+
+    public void setCPT(Hashtable<Integer,Color> cpt){
+        ColorPlayerTable = cpt;
+    }
+
     public int getWhoseMove () {
         return 0;
     }
+
 }
