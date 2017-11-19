2efd1773332d13cd628488fc9ebda26ff38e07a8
Sizhe Cao <cao20@up.edu>
2017-11-19 15:03:05 -0800

    int array

--- app/src/main/java/up/edu/cs301/chinese_checkers/Board.java
+++ app/src/main/java/up/edu/cs301/chinese_checkers/Board.java
@@ -6,40 +6,40 @@ package up.edu.cs301.chinese_checkers;
 
 public class Board {
     //create spaces in the board to hold marbles
-    public boolean[][] CCBoard = {
-            { false, false, false, false, false, false, true, false, false, false, false, false, false }, // row 0
+    public int[][] CCBoard = {
+            { -1, -1, -1, -1, -1, -1, 0, -1, -1, -1, -1, -1, -1 }, // row 0
 
-            { false, false, false, false, false,  true, true, false, false, false, false, false, false }, // row 1
+            { -1, -1, -1, -1, -1,  0, 0, -1, -1, -1, -1, -1, -1 }, // row 1
 
-            { false, false, false, false, false,  true, true,  true, false, false, false, false, false }, // row 2
+            { -1, -1, -1, -1, -1,  0, 0,  0, -1, -1, -1, -1, -1 }, // row 2
 
-            { false, false, false, false,  true,  true, true,  true, false, false, false, false, false }, // row 3
+            { -1, -1, -1, -1,  0,  0, 0,  0, -1, -1, -1, -1, -1 }, // row 3
 
-            {  true,  true,  true,  true,  true,  true, true,  true,  true,  true,  true,  true,  true }, // row 4
+            {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0 }, // row 4
 
-            {  true,  true,  true,  true,  true,  true, true,  true,  true,  true,  true,  true, false }, // row 5
+            {  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, -1 }, // row 5
 
-            { false,  true,  true,  true,  true,  true, true,  true,  true,  true,  true,  true, false }, // row 6
+            { -1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, -1 }, // row 6
 
-            { false,  true,  true,  true,  true,  true, true,  true,  true,  true,  true, false, false }, // row 7
+            { -1,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0, -1, -1 }, // row 7
 
-            { false, false,  true,  true,  true,  true, true,  true,  true,  true,  true, false, false }, // row 8 -----
+            { -1, -1,  0,  0,  0,  0, 0,  0,  0,  0,  0, -1, -1 }, // row 8 -----
 
-            { false,  true,  true,  true,  true,  true, true,  true,  true,  true,  true, false, false }, // row 7
+            { -1,  0,  0,  0,  0,  0, 0,  0,  0,  0,  0, -1, -1 }, // row 7
 
-            { false,  true,  true,  true,  true,  true, true,  true,  true,  true,  true,  true, false }, // row 6
+            { -1,  0,  0,  0,  0,  0, 0,  0,  0,  0,  0,  0, -1 }, // row 6
 
-            {  true,  true,  true,  true,  true,  true, true,  true,  true,  true,  true,  true, false }, // row 5
+            {  0,  0,  0,  0,  0,  0, 0,  0,  0,  0,  0,  0, -1 }, // row 5
 
-            {  true,  true,  true,  true,  true,  true, true,  true,  true,  true,  true,  true,  true }, // row 4
+            {  0,  0,  0,  0,  0,  0, 0,  0,  0,  0,  0,  0,  0 }, // row 4
 
-            { false, false, false, false,  true,  true, true,  true, false, false, false, false, false }, // row 3
+            { -1, -1, -1, -1,  0,  0, 0,  0, -1, -1, -1, -1, -1 }, // row 3
 
-            { false, false, false, false, false,  true, true,  true, false, false, false, false, false }, // row 2
+            { -1, -1, -1, -1, -1,  0, 0,  0, -1, -1, -1, -1, -1 }, // row 2
 
-            { false, false, false, false, false,  true, true, false, false, false, false, false, false }, // row 1
+            { -1, -1, -1, -1, -1,  0, 0, -1, -1, -1, -1, -1, -1 }, // row 1
 
-            { false, false, false, false, false, false, true, false, false, false, false, false, false }, // row 0
+            { -1, -1, -1, -1, -1, -1, 0, -1, -1, -1, -1, -1, -1 }, // row 0
     };
 
     public Board (){
