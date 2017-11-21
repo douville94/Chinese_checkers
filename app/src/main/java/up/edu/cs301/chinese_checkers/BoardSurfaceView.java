ceaf97000d6ab4ba79e316bfb74a7a28d414d0d2
Sizhe Cao <cao20@up.edu>
2017-11-20 19:03:33 -0800

    modify the board to be smaller

--- app/src/main/java/up/edu/cs301/chinese_checkers/BoardSurfaceView.java
+++ app/src/main/java/up/edu/cs301/chinese_checkers/BoardSurfaceView.java
@@ -375,7 +375,7 @@ public class BoardSurfaceView extends SurfaceView implements SurfaceHolder.Callb
         };
 
         int x = 200;
-        int y = 0;
+        int y =40;
         boolean offset = false;
 //        float[][] validPos = new[13][17];
         //iterate first row
@@ -408,7 +408,8 @@ public class BoardSurfaceView extends SurfaceView implements SurfaceHolder.Callb
                         break;
                 }
 
-                x+=100;
+                x+=95;
+                y-=1;
             }
             offset = !offset;
         }
